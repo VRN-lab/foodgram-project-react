2b51f523fb0f2f871c9bfaffec40739d18fd16a8
Viktor <alison2500@yandex.ru>
2023-04-15 22:55:41 +1200

    The deployment is configured

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,5 +1,3 @@
-from django import urls
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,15 +6,3 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/', include('users.urls')),
 ]
-
-if not settings.DEBUG:
-    urlpatterns += [
-        urls(
-            r'^media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
-        ),
-        urls(
-            r'^static/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.STATIC_ROOT}
-        ),
-    ]
